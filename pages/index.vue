4e3ca92761ffcd2ec662e3b451709ab175dc4514
pandupt <pandupersada73@gmail.com>
2023-09-17 22:28:03 +0700

    add form handler

--- pages/index.vue
+++ pages/index.vue
@@ -21,9 +21,9 @@
         </div>
       </div>
       <div clas="list-university row">
-        <!-- <CardItem :university="universities[0]" :isGrid="isGrid" />
-        <CardItem :university="universities[1]" :isGrid="isGrid" />
-        <CardItem :university="universities[2]" :isGrid="isGrid" /> -->
+        <!-- <CardItem :university="university[0]" :isGrid="isGrid" />
+        <CardItem :university="university[1]" :isGrid="isGrid" />
+        <CardItem :university="university[2]" :isGrid="isGrid" /> -->
         <CardItem
         v-for="(university, i) in resultQuery"
         :key="i"
@@ -34,16 +34,40 @@
       <div class="action py-2">
       <a v-if="!isCreating" href="#" class="add-button"  @click="isCreating = !isCreating">Add University</a>
       <div v-else class="add-card" >
-        <div class="card mb-2">
-          <div class="card-body d-flex flex-column p-0">
-            <input class="form-control border-0 mb-2" placeholder="University" type="text" >
-            <textarea class="form-control border-0 small" placeholder="Review" rows="3"></textarea>
+      <section class="form">
+        <form v-on:submit.prevent="handleSubmit">
+
+        <div class="field">
+          <div class="control">
+
+            <div class="card mb-2">
+              <div class="card-body d-flex flex-column p-0">
+                <input v-model="form.university" class="form-control border-0 mb-2" placeholder="University" type="text" >
+                <textarea v-model="form.review" class="form-control border-0 small" placeholder="Review" rows="3"></textarea>
+              </div>
+            </div>
           </div>
         </div>
           <div class="button-wrapper d-flex">
-              <button class="btn btn-primary me-2">Save</button>
-              <button class="btn btn-outline-secondary" @click="isCreating = !isCreating">Cancel</button>
+            <form v-on="handleSubmit">
+              <div class="field is-grouped">
+                <div class="control">
+
+                  <button class="btn btn-primary me-2">Save</button>
+                  <button class="btn btn-outline-secondary" @click="isCreating = !isCreating">Cancel</button>
+                </div>
+              </div>
+            </form>
           </div>
+        </form>
+      </section>
+      <div>
+        <ul>
+          <li v-for="item in inputanForm" :key="item">
+            {{ form }}
+          </li>
+        </ul>
+      </div>
       </div>
     </div>
   </div>
@@ -60,35 +84,34 @@ export default {
       searchQuery: '',
       isGrid: true,
       isCreating: false,  
-      universities: [
-        {
-        name: 'IPB University',
-        review: 'Bagus',
-        },
-        {
-        name: 'STT Nurul Fikri',
-        review: 'Bagus',
-        },
-        {
-        name: 'Universitas Pakuan',
-        review: 'Bagus',
-        }
-      ]
+      form : {
+        university: '',
+        review: ''
+      },
+      inputanForm: []
     }
   },
   computed: {
     resultQuery() {
       if (this.searchQuery) {
-        return this.universities.filter((item) => {
+        return this.university.filter((item) => {
           return this.searchQuery
           .toLowerCase()
           .split(" ")
           .every((v) => item.name.toLowerCase().includes(v));
         });
       } else {
-        console.log(this.universities)
-        return this.universities
+        console.log(this.university)
+        return this.university
       }
+    }
+  },
+  methods: {
+    handleSubmit() {
+      const item = {
+        form: this.form
+      }
+      this.inputanForm.push(item)
     }
   }
 }
